2204f71f8894659a3620344906abd4cb4c4d18bd
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-06-19 10:28:45 +0530

    error fixed add customer method

--- Foodies Fave Queue Management System/src/Main.java
+++ Foodies Fave Queue Management System/src/Main.java
@@ -6,11 +6,11 @@ public class Main {
     static String[] array1 = {"O", "O"};
     static String[] array2 = {"O", "O", "O"};
     static String[] array3 = {"O", "O", "O", "O", "O"};
-    static String[] cusNameArray1={};
-    static String[] cusNameArray2={};
-    static String[] cusNameArray3={};
+    static String[] cusNameArray1=new String[2];
+    static String[] cusNameArray2=new String[3];
+    static String[] cusNameArray3=new String[5];
 
-    static int burgerAmount=50;
+    static int burgerAmount=13;
 
     public static void main(String[] args) {
         int x = 0;
@@ -46,6 +46,8 @@ public class Main {
                     break;
                 case "105":
                 case "VCS":
+                    sortedCustomer();
+                    break;
                 case "106":
                 case "SPD":
                 case "107":
@@ -134,47 +136,71 @@ public class Main {
     }
 
     public static void addCustomers() {
-/*        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;*/
         try {
+            for (int v = 1; v > -1; v++) {
+                System.out.println("Enter Customer name  :");
+                String cName = scn.next();
 
-            System.out.println("Enter customer name  :");
-            String cusName = scn.next();
+                System.out.println("Enter Queue number you wish to add customer(1-3) or Press 0 to come back to menu  :");
+                int qNum = scn.nextInt();
 
-            if (array1[0] == "O") {
-                array1[0] = "X";
-                cusNameArray1[0]=cusName;
-                System.out.println("Customer added to queue 1");
-            } else if (array2[0] == "O") {
-                array2[0] = "X";
-                cusNameArray2[0]=cusName;
-                System.out.println("Customer added to queue 2");
-            } else if (array3[0] == "O") {
-                array3[0] = "X";
-                cusNameArray3[0]=cusName;
-                System.out.println("Customer added to queue 3");
-            } else {
+                if (qNum == 1) {
+                    if (array1[1] == "O") {
+                        for (int i = 0; i < 2; i++) {
+                            if (array1[i] == "O") {
+                                array1[i] = "X";
+                                cusNameArray1[i] = cName;
+                                System.out.println(cusNameArray1[i] + " added to the queue 1");
+                                break;
+                            }
+                        }
 
-                for (int i = 1; i < 5; i++) {
-                    if (array1[i] == "O") {
-                        array1[i] = "X";
-                        System.out.println("Customer added to queue 1");
-                        break;
-                    } else if (array2[i] == "O") {
-                        array2[i] = "X";
-                        System.out.println("Customer added to queue 2");
-                        break;
-                    } else if (array3[i] == "O") {
-                        array3[i] = "X";
-                        System.out.println("Customer added to queue 3");
-                        break;
                     } else {
-                        System.out.println("All queues are full");
+                        System.out.println("Queue 1 is full.Try a another queue.");
+                        break;
+
                     }
+
+                } else if (qNum == 2) {
+                    if (array2[2] == "O") {
+                        for (int i = 0; i < 3; i++) {
+                            if (array2[i] == "O") {
+                                array2[i] = "X";
+                                cusNameArray2[i] = cName;
+                                System.out.println(cusNameArray2[i] + " added to the queue 2");
+                                break;
+                            }
+                        }
+
+                    } else {
+                        System.out.println("Queue 2 is full.Try a another queue.");
+                        break;
+
+                    }
+                } else if (qNum == 3) {
+                    if (array3[4] == "O") {
+                        for (int i = 0; i < 5; i++) {
+                            if (array3[i] == "O") {
+                                array3[i] = "X";
+                                cusNameArray3[i] = cName;
+                                System.out.println(cusNameArray3[i] + " added to the queue 3");
+                                break;
+                            }
+                        }
+
+                    } else {
+                        System.out.println("Queue 3 is full.Try a another queue.");
+                        break;
+
+                    }
+                } else if (qNum == 0) {
+                    break;
+
+                } else {
+                    System.out.println("Invalid queue number.");
+                    break;
                 }
             }
-
         }catch (Exception e){
             System.out.println(e);
         }
@@ -240,20 +266,45 @@ public class Main {
             int cashNum = scn.nextInt();
 
             if(cashNum==1){
-                array1[0]="O";
-                burgerAmount-=5;
-                System.out.println("Removed the served customer");
+                if(array1[0]=="O"){
+                    System.out.println("There is no customer in the queue 1");
+                }else{
+                    for(int i=0;i<2;i++){
+                        array1[i]=array1[i+1];
+                    }
+                    burgerAmount-=5;
+                    System.out.println("Removed the served customer");
+                }
             }
-            if(cashNum==2){
-                array2[0]="O";
-                burgerAmount-=5;
-                System.out.println("Removed the served customer");
+            else if(cashNum==2){
+                if(array2[0]=="O"){
+                    System.out.println("There is no customer in the queue 2");
+                }else{
+                    for(int i=0;i<3;i++){
+                        array3[i]=array3[i+1];
+                    }
+                    burgerAmount-=5;
+                    System.out.println("Removed the served customer");
+                }
             }
-            if(cashNum==3){
-                array3[0]="O";
-                burgerAmount-=5;
-                System.out.println("Removed the served customer");
+            else if(cashNum==3){
+                if(array3[0]=="O"){
+                    System.out.println("There is no customer in the queue 3");
+                }else{
+                    for(int i=0;i<5;i++){
+                        array3[i]=array3[i+1];
+                    }
+                    burgerAmount-=5;
+                    System.out.println("Removed the served customer");
+                }
             }
+            else{
+                System.out.println("Invalid Input");
+            }
+            if (burgerAmount<=10){
+                System.out.println("****The stock of burgers is running low. Please replenish the stock.****");
+            }
+
         }catch (Exception e){
             System.out.println(e);
         }
@@ -270,5 +321,27 @@ public class Main {
         burgerAmount= burgerAmount+amnt;
         System.out.println("Burger stocks updated");
     }
+
+    public static void sortedCustomer(){
+        try{
+            String temp;
+            System.out.println("Strings in sorted order:");
+            for (int j = 0; j < cusNameArray1.length; j++) {
+                for (int i = j + 1; i < cusNameArray1.length; i++) {
+                    // comparing adjacent strings
+                    if (cusNameArray1[i].compareTo(cusNameArray1[j]) < 0) {
+                        temp = cusNameArray1[j];
+                        cusNameArray1[j] = cusNameArray1[i];
+                        cusNameArray1[i] = temp;
+                    }
+                }
+                System.out.println(cusNameArray1[j]);
+                System.out.println("Sorted");
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
 
 }
